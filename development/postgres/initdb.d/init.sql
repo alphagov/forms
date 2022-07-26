f92aed59d4012b04767d782c3f064d9500801c59
Tom Iles <tom.iles@engineering.digital.dwp.gov.uk>
2022-07-26 15:38:25 +0100

    Add docker compose setup
    This commit adds a docker compose development environment for the 3
    projects which make up the GOV.UK Forms service.
    
    It's only intended for use in development, not production.
    
    Notes:
     - uses version 2.4 because it's slightly better for development
     - uses Debian over Alpine, which is larger but perhaps has better
       support
     - exposes the individual service ports to the host
     - could use host.docker.internal in the var files to allow things like
       running the api natively while still using the other projects
       externally
     - single Dockerfile in the projects which defaults to being "production
       ready". The compose file overrides values for development

---
+++ development/postgres/initdb.d/init.sql
@@ -0,0 +1,7 @@
+CREATE USER forms_admin WITH PASSWORD 'forms_admin' CREATEDB;
+CREATE DATABASE forms_admin;
+GRANT ALL PRIVILEGES ON DATABASE forms_admin TO forms_admin;
+
+CREATE USER forms_api WITH PASSWORD 'forms_api' CREATEDB;
+CREATE DATABASE forms_api;
+GRANT ALL PRIVILEGES ON DATABASE forms_api TO forms_api;
